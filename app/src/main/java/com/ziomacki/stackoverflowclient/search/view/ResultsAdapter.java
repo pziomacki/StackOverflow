20213cc364188cb075defde5ece1a3f136b1277b
Piotr Ziomacki <piotr@macoscope.net>
2016-08-14 12:27:29 +0200

    Open browser with details page on item click

--- app/src/main/java/com/ziomacki/stackoverflowclient/search/view/ResultsAdapter.java
+++ app/src/main/java/com/ziomacki/stackoverflowclient/search/view/ResultsAdapter.java
@@ -1,12 +1,10 @@
 package com.ziomacki.stackoverflowclient.search.view;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.squareup.picasso.Picasso;
 import com.ziomacki.stackoverflowclient.R;
 import com.ziomacki.stackoverflowclient.search.model.SearchResultItem;
 
@@ -33,20 +31,7 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsViewHolder> {
     @Override
     public void onBindViewHolder(ResultsViewHolder holder, int position) {
         SearchResultItem resultItem = resultItemList.get(position);
-        holder.resultItemTitle.setText(resultItem.getTitle());
-        Context context = holder.avatar.getContext();
-        holder.answersCount.setText(context.getString(R.string.result_answer_count, resultItem.getAnswerCount()));
-        holder.ownerName.setText(resultItem.getOwner().getDisplayName());
-        String imageUrl = resultItem.getOwner().getProfileImage();
-        if (!isEmptyString(imageUrl)) {
-            Picasso.with(context)
-                    .load(resultItem.getOwner().getProfileImage())
-                    .into(holder.avatar);
-        }
-    }
-
-    private boolean isEmptyString(String string) {
-        return string == null || string.equals("");
+        holder.bind(resultItem);
     }
 
     @Override
--- app/src/main/java/com/ziomacki/stackoverflowclient/search/view/ResultsViewHolder.java
+++ app/src/main/java/com/ziomacki/stackoverflowclient/search/view/ResultsViewHolder.java
@@ -1,11 +1,17 @@
 package com.ziomacki.stackoverflowclient.search.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.ziomacki.stackoverflowclient.R;
+import com.ziomacki.stackoverflowclient.search.model.SearchResultItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,9 +26,48 @@ public class ResultsViewHolder extends RecyclerView.ViewHolder {
     TextView ownerName;
     @BindView(R.id.result_item_avatar)
     ImageView avatar;
+    @BindView(R.id.result_item_container)
+    RelativeLayout mainContainer;
+
+    SearchResultItem resultItem;
 
     public ResultsViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(SearchResultItem resultItem) {
+        this.resultItem = resultItem;
+        resultItemTitle.setText(resultItem.getTitle());
+        Context context = avatar.getContext();
+        answersCount.setText(context.getString(R.string.result_answer_count, resultItem.getAnswerCount()));
+        ownerName.setText(resultItem.getOwner().getDisplayName());
+        loadAvatar(avatar, context);
+        setOnclickListener();
+    }
+
+    private void loadAvatar(ImageView imageView, Context context) {
+        String imageUrl = resultItem.getOwner().getProfileImage();
+        if (!isEmptyString(imageUrl)) {
+            Picasso.with(context)
+                    .load(resultItem.getOwner().getProfileImage())
+                    .into(imageView);
+        }
+    }
+
+    private boolean isEmptyString(String string) {
+        return string == null || string.equals("");
+    }
+
+    private void setOnclickListener() {
+        if (!isEmptyString(resultItem.getLink())) {
+            mainContainer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(resultItem.getLink()));
+                    view.getContext().startActivity(intent);
+                }
+            });
+        }
     }
 }
